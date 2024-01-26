be97369be3b971313e8ef21348780fc8d3b00924
Askhad Arzybaev <aarzybaev@gmail.com>
2024-01-26 22:05:30 +0600

    created MessageItem component

---
+++ src/components/MessageItem/MessageItem.tsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import dayjs from 'dayjs';
+interface Props {
+  author: string;
+  message: string;
+  date: string;
+}
+
+const MessageItem: React.FC<Props> = ({
+                                        author,
+                                        message,
+                                        date
+                                      }) => {
+  return (
+    <div className="card mb-2">
+      <div className="card-header">
+        <div className="d-flex justify-content-between">
+          <span>{author}</span><span>{dayjs(date).format('DD-MMM, YYYY HH:MM')}</span>
+        </div>
+      </div>
+      <div className="card-body">
+        <p className="card-text">{message}</p>
+      </div>
+    </div>
+  );
+};
+
+export default MessageItem;
